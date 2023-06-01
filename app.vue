88181babc39f81c29c635b94145d91ee7cf435ad
robustcicada <suda@saice.co.jp>
2023-06-01 11:15:26 +0900

    Header, Navigationを追加

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <p>いらっしゃい!</p>
+  <div class="flex flex-col h-screen">
+    <Header />
+    <div class="flex flex-grow">
+      <Navigation />
+      <NuxtPage />
+    </div>
   </div>
 </template>
